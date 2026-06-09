6d3efab5bcd216e9335f7775455ef12e023aa462
RayFarias13 <1313@gmail.com>
2026-06-09 14:43:35 -0300

    09/06 4

--- apiinfo/urls.py
+++ apiinfo/urls.py
@@ -1,22 +1,49 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-
 from .views import (
     UserViewSet,
     AlunoViewSet,
     AdaptacaoViewSet,
-    RelatorioViewSet
+    RelatorioViewSet,
 )
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'alunos', AlunoViewSet)
-router.register(r'adaptacoes', AdaptacaoViewSet)
-router.register(r'relatorios', RelatorioViewSet)
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'alunos', AlunoViewSet, basename='alunos')
+router.register(r'adaptacoes', AdaptacaoViewSet, basename='adaptacoes')
+router.register(r'relatorios', RelatorioViewSet, basename='relatorios')
 
 urlpatterns = [
-    
-
     path('', include(router.urls)),
+
+    path(
+        'adaptacoes/aluno/<int:student_id>/',
+        AdaptacaoViewSet.as_view({'get': 'list_by_student'}),
+        name='adaptacoes-por-aluno'
+    ),
+
+    path(
+        'adaptacoes/<int:student_id>/<int:pk>/',
+        AdaptacaoViewSet.as_view({
+            'put': 'update_by_student',
+            'delete': 'destroy_by_student',
+        }),
+        name='adaptacao-aluno-detalhe'
+    ),
+
+    path(
+        'relatorios/aluno/<int:student_id>/',
+        RelatorioViewSet.as_view({'get': 'list_by_student'}),
+        name='relatorios-por-aluno'
+    ),
+
+    path(
+        'relatorios/<int:student_id>/<int:pk>/',
+        RelatorioViewSet.as_view({
+            'put': 'update_by_student',
+            'delete': 'destroy_by_student',
+        }),
+        name='relatorio-aluno-detalhe'
+    ),
 ]
--- apiinfo/views.py
+++ apiinfo/views.py
@@ -1,31 +1,72 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .models import UserModel, AlunoModel, AdaptationsModel, ReportsModel
-from .serializers import UserSerializer, AlunoSerializer, AdaptacaoSerializer, RelatorioSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import (UserModel, AlunoModel, AdaptationsModel, ReportsModel)
+
+from .serializers import (UserSerializer, AlunoSerializer, AdaptacaoSerializer,RelatorioSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = UserModel.objects.all().order_by('id')
     serializer_class = UserSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['id', 'user__username', 'user__email']  
+    filterset_fields = [
+        'id',
+        'user__username',
+        'user__email'
+    ]
+
 
 class AlunoViewSet(viewsets.ModelViewSet):
     queryset = AlunoModel.objects.all().order_by('id')
     serializer_class = AlunoSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['id', 'name', 'course']  
+    filterset_fields = [
+        'id',
+        'name',
+        'course'
+    ]
+
 
 class AdaptacaoViewSet(viewsets.ModelViewSet):
     queryset = AdaptationsModel.objects.all().order_by('id')
     serializer_class = AdaptacaoSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['id', 'student__name', 'description']  
+    filterset_fields = [
+        'id',
+        'student',
+        'student__name',
+        'description'
+    ]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        student_id = self.request.query_params.get('student')
+
+        if student_id:
+            queryset = queryset.filter(student_id=student_id)
+
+        return queryset
+
 
 class RelatorioViewSet(viewsets.ModelViewSet):
     queryset = ReportsModel.objects.all().order_by('id')
     serializer_class = RelatorioSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['id', 'student__name', 'description']  
+    filterset_fields = [
+        'id',
+        'student',
+        'student__name',
+        'description'
+    ]
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        student_id = self.request.query_params.get('student')
+
+        if student_id:
+            queryset = queryset.filter(student_id=student_id)
+
+        return queryset
